10179c17832e0d8e2d83fc1d6bd8af4536fe5cf0
z-striker317 <95532421+z-striker317@users.noreply.github.com>
2022-10-11 00:56:08 +0530

    Create GuessNumber.java

---
+++ GuessNumber.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class GuessNum
+{
+public static void main(String[] args)
+{
+Scanner sc =new Scanner(System.in);
+int num = (int)(Math.random()*100);
+int unum=0;
+do
+{
+System.out.println("Guess the number:");
+int unum= sc.nextInt();
+
+if(usernum==num)
+{
+System.out.println("Correct");
+break;
+}
+
+else if (isernum>num)
+{
+System.out.println("Your num is greater");
+}
+else 
+{
+System.out.println("Your num is less");
+}
+}
+while(unum>=0);
+}
+System.out.println("The num was" + num);
+}
+}
